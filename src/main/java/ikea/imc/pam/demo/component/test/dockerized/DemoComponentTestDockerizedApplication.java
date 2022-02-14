2fc7e644ebcd0ba69812569cc643c08c6e94b88c
David Strömner <david.stromner@inter.ikea.com>
2022-02-14 08:16:22 +0100

    Initial commit

---
+++ src/main/java/ikea/imc/pam/demo/component/test/dockerized/DemoComponentTestDockerizedApplication.java
@@ -0,0 +1,13 @@
+package ikea.imc.pam.demo.component.test.dockerized;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoComponentTestDockerizedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoComponentTestDockerizedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ikea/imc/pam/demo/component/test/dockerized/DemoComponentTestDockerizedApplicationTests.java
@@ -0,0 +1,13 @@
+package ikea.imc.pam.demo.component.test.dockerized;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoComponentTestDockerizedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
